83d1b14624168f72b367fc9ac5c164233da8d2fc
Dokyeongyun <aservmz@naver.com>
2022-07-11 20:07:33 +0900

    Add model(DTO) for paging and search history api

---
+++ src/main/java/com/coders/laundry/dto/SearchHistory.java
@@ -0,0 +1,14 @@
+package com.coders.laundry.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class SearchHistory {
+    private int searchKeywordId;
+    private String keyword;
+    private LocalDateTime createdAt;
+}
