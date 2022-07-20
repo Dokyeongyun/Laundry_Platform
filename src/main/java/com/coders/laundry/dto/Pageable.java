17114ca560175ab825c4759ab5b9d686b7ccbd73
Dokyeongyun <aservmz@naver.com>
2022-07-20 19:09:26 +0900

    Assign validation constraints to DTO models

--- src/main/java/com/coders/laundry/dto/Pageable.java
+++ src/main/java/com/coders/laundry/dto/Pageable.java
@@ -1,12 +1,29 @@
 package com.coders.laundry.dto;
 
-import lombok.*;
+import com.coders.laundry.common.validation.ValidEnum;
+import com.coders.laundry.domain.enums.SortType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.PositiveOrZero;
 
 @Data
 @AllArgsConstructor
 public class Pageable {
+
+    @PositiveOrZero
     private int offset;
+
+    @Range(min = 0, max = 100)
     private int limit;
+
     private String sort;
+
+    @ValidEnum(enumClass = SortType.class)
     private String sortType;
+
+    public Pageable() {
+        this.sortType = "asc";
+    }
 }
