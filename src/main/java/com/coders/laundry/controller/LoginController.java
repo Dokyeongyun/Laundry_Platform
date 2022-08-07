c847d96478d3eda8215279209e450da1ec6eeec9
dldmldlsy <dldmldlsy@naver.com>
2022-08-07 16:38:58 +0900

    LoginController 수정: @RestController 추가, 가독성 개선(개행문자, {}이용), @RequiredArgsConstructor을 통한 생성자주입, 불필요한 주석 제거

--- src/main/java/com/coders/laundry/controller/LoginController.java
+++ src/main/java/com/coders/laundry/controller/LoginController.java
@@ -3,30 +3,33 @@ package com.coders.laundry.controller;
 import com.coders.laundry.dto.LoginRequest;
 import com.coders.laundry.dto.LoginResponse;
 import com.coders.laundry.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
 public class LoginController {
-    private LoginService loginService;
-    @Autowired
-    LoginController(LoginService loginService){
-        this.loginService = loginService;
-    }
+
+    private final LoginService loginService;
+
     @PostMapping("/api/login")
     public ResponseEntity login(@RequestBody LoginRequest loginInfo){
+
         String phoneNum = loginInfo.getPhoneNum();
-        String pw = loginInfo.getPassword();
-        //DB에 있는지 확인.
-        if (!loginService.existMember(phoneNum))
+        String password = loginInfo.getPassword();
+
+        if (!loginService.existMember(phoneNum)) {
             return new ResponseEntity<>("존재하지않는 ID입니다.", HttpStatus.BAD_REQUEST);
-        //비밀번호 맞는지 확인.
-        if (!loginService.matchPW(phoneNum, pw))
+        }
+
+        if (!loginService.matchPW(phoneNum, password)) {
             return new ResponseEntity<>("비밀번호가 틀렸습니다.", HttpStatus.BAD_REQUEST);
+        }
         LoginResponse loginResponse = loginService.getLoginResponse(phoneNum);
         return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
-
 }
