d0bb9d806eb82ac0cfd7a427880c4a249925bd79
dldmldlsy <dldmldlsy@naver.com>
2022-08-16 20:23:59 +0900

    로그인 api 수정: token유효시간값변경, 로그인요청DTO 생성자어노테이션추가, API요청응답시의 타입설정(JSON)

--- src/main/java/com/coders/laundry/controller/LoginController.java
+++ src/main/java/com/coders/laundry/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.coders.laundry.dto.LoginResponse;
 import com.coders.laundry.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,8 @@ public class LoginController {
 
     private final LoginService loginService;
 
-    @PostMapping("/api/login")
+    @PostMapping(value = "/api/login", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginInfo){
 
         String phoneNum = loginInfo.getPhoneNum();
@@ -25,9 +27,9 @@ public class LoginController {
         LoginResponse loginResponse = loginService.login(phoneNum, password);
 
         if (loginResponse==null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
 
     }
 }
--- src/main/java/com/coders/laundry/dto/LoginRequest.java
+++ src/main/java/com/coders/laundry/dto/LoginRequest.java
@@ -1,13 +1,14 @@
 package com.coders.laundry.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
-
     String phoneNum;
     String password;
-
 }
