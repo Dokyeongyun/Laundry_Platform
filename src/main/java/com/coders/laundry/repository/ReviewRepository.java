71eef610535046638290f4b7e627a0489f9c42ba
Dokyeongyun <aservmz@naver.com>
2022-09-06 19:10:48 +0900

    TDD: Add method that selecting and deleting review entity

--- src/main/java/com/coders/laundry/repository/ReviewRepository.java
+++ src/main/java/com/coders/laundry/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.coders.laundry.repository;
 
 import com.coders.laundry.domain.entity.ReviewEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
@@ -9,5 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface ReviewRepository {
 
     int insert(ReviewEntity review);
+
+    ReviewEntity selectById(@Param("reviewId") int reviewId);
+
+    int deleteById(@Param("reviewId") int reviewId);
 
 }
--- src/test/java/com/coders/laundry/repository/ReviewRepositoryTest.java
+++ src/test/java/com/coders/laundry/repository/ReviewRepositoryTest.java
@@ -30,11 +30,56 @@ class ReviewRepositoryTest {
                 .build();
 
         // Act
-        int insertedRowCount = reviewRepository.insert(entity);
+        int result = reviewRepository.insert(entity);
 
         // Assert
-        assertEquals(1, insertedRowCount);
+        assertEquals(1, result);
         assertNotNull(entity.getReviewId());
+    }
+
+    @Test
+    void selectById() {
+        // Arrange
+        ReviewEntity entity = ReviewEntity.builder()
+                .laundryId(1)
+                .writerId(1)
+                .contents("이 빨래방은 정말.....")
+                .rating(5)
+                .build();
+
+        reviewRepository.insert(entity);
+
+        // Act
+        ReviewEntity result = reviewRepository.selectById(entity.getReviewId());
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(entity.getReviewId(), result.getReviewId());
+        assertEquals(entity.getLaundryId(), result.getLaundryId());
+        assertEquals(entity.getWriterId(), result.getWriterId());
+        assertEquals(entity.getContents(), result.getContents());
+        assertEquals(entity.getRating(), result.getRating());
+        assertNotNull(result.getCreateDate());
+        assertNotNull(result.getUpdateDate());
+    }
+
+    @Test
+    void deleteById() {
+        // Arrange
+        ReviewEntity entity = ReviewEntity.builder()
+                .laundryId(1)
+                .writerId(1)
+                .contents("이 빨래방은 정말.....")
+                .rating(5)
+                .build();
+
+        reviewRepository.insert(entity);
+
+        // Act
+        int result = reviewRepository.deleteById(entity.getReviewId());
+
+        // Assert
+        assertEquals(1, result);
     }
 
 }
